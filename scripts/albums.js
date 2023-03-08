a9e2bb569ebcc0ead9ac704c06f22f90107847d7
Hunter-Santos <92822256+Hunter-Santos@users.noreply.github.com>
2023-03-08 13:20:13 -0600

    Update albums.js

--- scripts/albums.js
+++ scripts/albums.js
@@ -1,8 +1,62 @@
-/*
-  Create an array of 'album' objects.
-  Each object should have the following properties: 
-  "name", "artist", and "genre".
-
-  Optionally, you can also add an "artwork" property with an image of the album cover.
-*/
-
+let albums = [
+  {
+    name: "Thriller",
+    artist: "Michael Jackson",
+    genre: "Pop",
+    photo:'img/MJ.jpeg'
+},
+{
+  name: "Pyromania",
+  artist: "Def Leppard",
+  genre: "Rock",
+  photo: 'img/DL.jpeg'
+},
+{
+  name: "Combat Rock",
+  artist: "The Clash",
+  genre: "Punk Rock",
+  photo: 'img/CL.jpeg'
+},
+{
+  name: "Some Great Reward",
+  artist: "Depeche Mode",
+  genre: "Synth-Pop",
+  photo: 'img/DM.jpeg'
+},
+{
+  name: "Back in Black",
+  artist: "AC/DC",
+  genre: "Hard Rock",
+  photo: 'img/ACDC.png'
+},
+{
+  name: "Master of Puppets",
+  artist: "Metallica",
+  genre: "Heavy Metal",
+  photo: 'img/MET.jpeg'
+},
+{
+  name: "She's So Unusal",
+  artist: "Cyndi Lauper",
+  genre: "Pop",
+  photo:'img/CL.jpeg'
+},
+{
+  name: "Pleasant Dreams",
+  artist: "Ramones",
+  genre: "Punk Rock",
+  photo: 'img/RAM.jpeg'
+},
+{
+  name: "Escape",
+  artist: "Journey",
+  genre: "Rock",
+  photo: 'img/ESC.jpeg'
+},
+{
+  name: "Moving Picture",
+  artist: "Rush",
+  genre: "Hard",
+  photo:'img/RUSH.jpeg'
+}
+];
