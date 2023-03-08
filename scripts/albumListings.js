9cc3574d73983fc2b726eadc18ef4aac73ecb9d0
Hunter-Santos <92822256+Hunter-Santos@users.noreply.github.com>
2023-03-08 13:19:47 -0600

    Update albumListings.js

--- scripts/albumListings.js
+++ scripts/albumListings.js
@@ -1,16 +1,16 @@
-// Create a for loop that will list all of the albums in albums.js
-// If there are less than 10 albums, an <h4> message should be populated in the header that says "You need more albums!"
-// Otherwise, an <h4> message should be populated in the header that says "Let's start playing music!"
-
-// You already have the variable "html" that can be used to create the album listings for the DOM, 
-// as well as the code to produce whatever content the variable "html" has
-
-//The message variables have also been included
-
 let html = '';
 let moreAlbumsMessage = "You need more albums!";
 let playMusicMessage = "Let's start playing music!";
 
-// insert your code here
+
+for (let i = 0; i < albums.length; i++ ) {
+    html += `<h2>Album Name: ${albums[i].name} </h2>
+    <h3>Artist: ${albums[i].artist} </h3>
+    <p>Genre: ${albums[i].genre} </p>
+    <img scr="${albums.photo}" alt="${albums.name}">`
+    ;
+}
+
+document.querySelector('beforebegin', moreAlbumsMessage);
 
 document.querySelector('main').insertAdjacentHTML('beforeend', html);
